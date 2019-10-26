f92683c221e199cfbfb3ab427fd7201693a09660
GedMarc <ged_marc@hotmail.com>
2019-10-26 10:10:50 +0200

    Guiced EE Migration to its own space

--- src/main/java/com/guicedee/guicedservlets/rest/GuicedCXFNonSpringJaxrsServlet.java
+++ src/main/java/com/guicedee/guicedservlets/rest/GuicedCXFNonSpringJaxrsServlet.java
@@ -1,96 +1,99 @@
 package com.guicedee.guicedservlets.rest;
 
-import com.google.inject.Singleton;
-import org.apache.cxf.jaxrs.lifecycle.PerRequestResourceProvider;
-import org.apache.cxf.jaxrs.lifecycle.ResourceProvider;
-import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
-import org.apache.cxf.jaxrs.servlet.CXFNonSpringJaxrsServlet;
-import com.guicedee.guicedinjection.GuiceContext;
-import com.guicedee.logger.LogFactory;
+import com.google.inject.*;
+import com.guicedee.guicedinjection.*;
+import org.apache.cxf.jaxrs.lifecycle.*;
+import org.apache.cxf.jaxrs.servlet.*;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.servlet.*;
+import java.util.*;
+import java.util.logging.*;
 
 @Singleton
-public class GuicedCXFNonSpringJaxrsServlet extends CXFNonSpringJaxrsServlet {
-	private static final Logger log = LogFactory.getLog("GuicedCXFNonSpringJaxrsServlet");
-
-
+public class GuicedCXFNonSpringJaxrsServlet
+		extends CXFNonSpringJaxrsServlet
+{
+	private static final Logger log = com.guicedee.logger.LogFactory.getLog("GuicedCXFNonSpringJaxrsServlet");
 
 
 	protected Map<Class<?>, java.util.Map<String, List<String>>> getServiceClasses(ServletConfig servletConfig, boolean modelAvailable, String splitChar) throws
-			ServletException {
+			ServletException
+	{
 		String serviceBeans = RESTContext.renderPathServices();
-		if (serviceBeans == null) {
-			if (modelAvailable) {
-				return Collections.emptyMap();
-			} else {
-				return new HashMap<>();
+		String[] classNames = serviceBeans.split(splitChar);
+		Map<Class<?>, Map<String, List<String>>> map = new HashMap<>();
+		int len$ = classNames.length;
+		for (int i$ = 0; i$ < len$; ++i$)
+		{
+			String cName = classNames[i$];
+			Map<String, List<String>> props = new HashMap<>();
+			String theName = this.getClassNameAndProperties(cName, props);
+			if (theName.length() != 0)
+			{
+				Class<?> cls = this.loadClass(theName);
+				map.put(cls, props);
 			}
-		} else {
-			String[] classNames = serviceBeans.split(splitChar);
-			Map<Class<?>, Map<String, List<String>>> map = new HashMap<>();
-			int len$ = classNames.length;
-			for(int i$ = 0; i$ < len$; ++i$) {
-				String cName = classNames[i$];
-				Map<String, List<String>> props = new HashMap<>();
-				String theName = this.getClassNameAndProperties(cName, props);
-				if (theName.length() != 0) {
-					Class<?> cls = this.loadClass(theName);
-					map.put(cls, props);
-				}
-			}
-			if (map.isEmpty()) {
-				log.warning("No JaxRS Resource Class was found");
-
-			}
-			return map;
 		}
+		if (map.isEmpty())
+		{
+			log.warning("No JaxRS Resource Class was found");
+
+		}
+		return map;
+
 	}
 
 
-	protected Map<Class<?>, ResourceProvider> getResourceProviders(ServletConfig servletConfig, Map<Class<?>, Map<String, List<String>>> resourceClasses) throws ServletException {
-		String scope = servletConfig.getServletContext().getInitParameter("jaxrs.scope");
-		if (scope != null && !"singleton".equals(scope) && !"prototype".equals(scope)) {
+	protected Map<Class<?>, ResourceProvider> getResourceProviders(ServletConfig servletConfig, Map<Class<?>, Map<String, List<String>>> resourceClasses) throws ServletException
+	{
+		String scope = servletConfig.getServletContext()
+		                            .getInitParameter("jaxrs.scope");
+		if (scope != null && !"singleton".equals(scope) && !"prototype".equals(scope))
+		{
 			throw new ServletException("Only singleton and prototype scopes are supported");
-		} else {
+		}
+		else
+		{
 			boolean isPrototype = "prototype".equals(scope);
 			Map<Class<?>, ResourceProvider> map = new HashMap();
-			Iterator i$ = resourceClasses.entrySet().iterator();
+			Iterator i$ = resourceClasses.entrySet()
+			                             .iterator();
 
-			while(i$.hasNext()) {
-				Map.Entry<Class<?>, Map<String, List<String>>> entry = (Map.Entry)i$.next();
+			while (i$.hasNext())
+			{
+				Map.Entry<Class<?>, Map<String, List<String>>> entry = (Map.Entry) i$.next();
 				Class<?> c = entry.getKey();
-				map.put(c, isPrototype ? new PerRequestResourceProvider(c) : new SingletonResourceProvider(this.createSingletonInstance(c, (Map)entry.getValue(), servletConfig), true));
+				map.put(c, isPrototype
+				           ? new PerRequestResourceProvider(c)
+				           : new SingletonResourceProvider(this.createSingletonInstance(c, (Map) entry.getValue(), servletConfig), true));
 			}
 			return map;
 		}
 	}
 
 
-	protected Object createSingletonInstance(Class<?> cls, Map<String, List<String>> props, ServletConfig sc) throws ServletException {
-		try {
-			return GuiceContext.get(cls);
-		}catch(Throwable T)
+	protected Object createSingletonInstance(Class<?> cls, Map<String, List<String>> props, ServletConfig sc) throws ServletException
+	{
+		try
 		{
-			log.log(Level.SEVERE,"Unable to construct instance for cxf",T);
+			return GuiceContext.get(cls);
+		}
+		catch (Throwable T)
+		{
+			log.log(Level.SEVERE, "Unable to construct instance for cxf", T);
 			return null;
 		}
 	}
 
-	private String getClassNameAndProperties(String cName, Map<String, List<String>> props) {
+	private String getClassNameAndProperties(String cName, Map<String, List<String>> props)
+	{
 		String theName = cName.trim();
 		int ind = theName.indexOf("(");
-		if (ind != -1 && theName.endsWith(")")) {
+		if (ind != -1 && theName.endsWith(")"))
+		{
 			props.putAll(parseMapListSequence(theName.substring(ind + 1, theName.length() - 1)));
-			theName = theName.substring(0, ind).trim();
+			theName = theName.substring(0, ind)
+			                 .trim();
 		}
 		return theName;
 	}
