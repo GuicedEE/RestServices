3bb701c0adce930666da48904a5b345ca30b8506
GedMarc <ged_marc@hotmail.com>
2019-11-06 00:31:48 +0200

    Slight changes to service loading for unit tests

---
+++ src/test/java/com/guicedee/guiced/rest/RestEasyModuleTest.java
@@ -0,0 +1,44 @@
+package com.guicedee.guiced.rest;
+
+import com.guicedee.guicedinjection.GuiceContext;
+import com.guicedee.guicedservlets.undertow.GuicedUndertow;
+import com.guicedee.logger.LogFactory;
+import io.undertow.Undertow;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.logging.Level;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestEasyModuleTest
+{
+
+	@Test
+	void configureServlets() throws Exception
+	{
+		GuiceContext.instance()
+		            .getConfig()
+		            .setServiceLoadWithClassPath(true);
+		LogFactory.configureConsoleColourOutput(Level.INFO);
+		Undertow undertow = GuicedUndertow.boot("0.0.0.0", 6003);
+
+		//Do stuff
+		HttpClient client = HttpClient.newBuilder()
+		                              .connectTimeout(Duration.of(5, ChronoUnit.SECONDS))
+		                              .build();
+		HttpResponse response = client.send(HttpRequest.newBuilder()
+		                                               .GET()
+		                                               .uri(new URI("http://localhost:6003/rest/hello/world"))
+		                                               .build(),
+		                                    HttpResponse.BodyHandlers.discarding());
+
+		assertEquals(200, response.statusCode());
+		undertow.stop();
+	}
+}
