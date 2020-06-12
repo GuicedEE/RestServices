5847482ca2ca0f4231c995b0f4234cc95227ba00
GedMarc <ged_marc@hotmail.com>
2020-06-12 06:33:41 +0200

    clean split between JDK 8 and jpms builds - cleanup and service loaders, io to shading and lots more

---
+++ src/jre8/java/com/guicedee/guicedservlets/rest/RESTContext.java
@@ -0,0 +1,103 @@
+package com.guicedee.guicedservlets.rest;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.guicedee.guicedservlets.rest.implementations.JAXBMarshaller;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RESTContext
+{
+	private static final Set<String> pathServices = new HashSet<>();
+	private static final Set<String> providers = new HashSet<>();
+	private static final Set<String> inInterceptors = new HashSet<>();
+	private static final Set<String> outInterceptors = new HashSet<>();
+	private static final Set<String> outFaultInterceptors = new HashSet<>();
+	private static final Set<String> properties = new HashSet<>();
+	private static final Set<String> applications = new HashSet<>();
+	/**
+	 * Provides the url that the module will use to provide Web Services.
+	 * Does not default to module name, default to WebServices
+	 * <p>
+	 *
+	 * <p>
+	 * e.g. http://localhost/WebServices/helloworld
+	 */
+	public static String baseWSUrl = "rest";
+	/**
+	 * Whether to register all the providers found on the path.. warning - this has a tendency to enable oauth everywhere
+	 */
+	public static boolean autoRegisterProviders = false;
+
+	static
+	{
+		providers.add(JacksonJsonProvider.class.getCanonicalName());
+		providers.add(JacksonJaxbJsonProvider.class.getCanonicalName());
+		providers.add(JAXBMarshaller.class.getCanonicalName());
+		providers.add("org.apache.cxf.jaxrs.provider.JAXBElementProvider");
+		providers.add("org.apache.cxf.jaxrs.validation.JAXRSBeanValidationInvoker");
+		providers.add("com.guicedee.guicedservlets.rest.services.JavaTimeTypesParamConverterProvider");
+		providers.add("org.apache.cxf.jaxrs.validation.JAXRSParameterNameProvider");
+	}
+
+	static
+	{
+		inInterceptors.add("org.apache.cxf.jaxrs.validation.JAXRSBeanValidationInInterceptor");
+	}
+
+	public static String renderServices(Set<String> values)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (String pathService : values)
+		{
+			sb.append(pathService + ",");
+		}
+		if (!values.isEmpty())
+		{
+			sb = sb.deleteCharAt(sb.length() - 1);
+		}
+		return sb.toString();
+	}
+
+	public static Set<String> getPathServices()
+	{
+		return pathServices;
+	}
+
+	public static Set<String> getProviders()
+	{
+		return providers;
+	}
+
+	public static Set<String> getInInterceptors()
+	{
+		return inInterceptors;
+	}
+
+	public static Set<String> getOutInterceptors()
+	{
+		return outInterceptors;
+	}
+
+	public static Set<String> getProperties()
+	{
+		return properties;
+	}
+
+	public static Set<String> getApplications()
+	{
+		return applications;
+	}
+
+	/**
+	 * Getter for property 'outFaultInterceptors'.
+	 *
+	 * @return Value for property 'outFaultInterceptors'.
+	 */
+	public static Set<String> getOutFaultInterceptors()
+	{
+		return outFaultInterceptors;
+	}
+}
