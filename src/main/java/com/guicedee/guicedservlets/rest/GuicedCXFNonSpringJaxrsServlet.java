b23363486f1843b2c6c64b97234ca6054b2e0c79
GedMarc <ged_marc@hotmail.com>
2020-10-21 01:16:25 +0200

    zstd-jni update, jwebmp plugins updates and cleanups
    * Jakarta EL to version 4.0
    * ZSDTD-JNI Update

--- src/main/java/com/guicedee/guicedservlets/rest/GuicedCXFNonSpringJaxrsServlet.java
+++ src/main/java/com/guicedee/guicedservlets/rest/GuicedCXFNonSpringJaxrsServlet.java
@@ -9,6 +9,7 @@ import org.apache.cxf.jaxrs.servlet.*;
 import org.apache.cxf.message.Message;
 
 import javax.servlet.*;
+import java.lang.reflect.Modifier;
 import java.util.*;
 import java.util.logging.*;
 
@@ -17,8 +18,7 @@ import static com.guicedee.guicedservlets.rest.services.JaxRsPreStartup.*;
 
 @Singleton
 public class GuicedCXFNonSpringJaxrsServlet
-		extends CXFNonSpringJaxrsServlet
-{
+		extends CXFNonSpringJaxrsServlet {
 	private static final Logger log = com.guicedee.logger.LogFactory.getLog("GuicedCXFNonSpringJaxrsServlet");
 
 	protected List<?> getProviders(ServletConfig servletConfig, String splitChar) throws ServletException {
@@ -40,9 +40,9 @@ public class GuicedCXFNonSpringJaxrsServlet
 	}
 
 	protected void setInterceptors(JAXRSServerFactoryBean bean, ServletConfig servletConfig,
-	                               String paramName,
-	                               String splitChar) throws ServletException {
-		String value  = servletConfig.getServletContext().getInitParameter(paramName);
+								   String paramName,
+								   String splitChar) throws ServletException {
+		String value = servletConfig.getServletContext().getInitParameter(paramName);
 		if (value == null) {
 			return;
 		}
@@ -55,7 +55,7 @@ public class GuicedCXFNonSpringJaxrsServlet
 				try {
 					Class<?> intClass = loadClass(theValue, "Interceptor");
 					Object object = GuiceContext.get(intClass);
-					list.add((Interceptor<? extends Message>)object);
+					list.add((Interceptor<? extends Message>) object);
 				} catch (ServletException ex) {
 					throw ex;
 				} catch (Exception ex) {
@@ -77,25 +77,21 @@ public class GuicedCXFNonSpringJaxrsServlet
 
 
 	protected Map<Class<?>, java.util.Map<String, List<String>>> getServiceClasses(ServletConfig servletConfig, boolean modelAvailable, String splitChar) throws
-			ServletException
-	{
+			ServletException {
 		String serviceBeans = RESTContext.renderServices(getPathServices());
 		String[] classNames = serviceBeans.split(splitChar);
 		Map<Class<?>, Map<String, List<String>>> map = new HashMap<>();
 		int len$ = classNames.length;
-		for (int i$ = 0; i$ < len$; ++i$)
-		{
+		for (int i$ = 0; i$ < len$; ++i$) {
 			String cName = classNames[i$];
 			Map<String, List<String>> props = new HashMap<>();
 			String theName = this.getClassNameAndProperties(cName, props);
-			if (theName.length() != 0)
-			{
+			if (theName.length() != 0) {
 				Class<?> cls = this.loadClass(theName);
 				map.put(cls, props);
 			}
 		}
-		if (map.isEmpty())
-		{
+		if (map.isEmpty()) {
 			log.warning("No JaxRS Resource Class was found");
 
 		}
@@ -103,79 +99,92 @@ public class GuicedCXFNonSpringJaxrsServlet
 
 	}
 
+	private static final Set<String> bannedResources =new HashSet<>();
+	static {
+	    bannedResources.add("org.apache.cxf.rs.security.saml.sso.AbstractSSOSpHandler");
+	    bannedResources.add("org.apache.cxf.rs.security.saml.sso.RequestAssertionConsumerService");
+	    bannedResources.add("org.apache.cxf.rs.security.saml.sso.MetadataService");
+    }
 
 	@SuppressWarnings("unchecked")
-	protected Map<Class<?>, ResourceProvider> getResourceProviders(ServletConfig servletConfig, Map<Class<?>, Map<String, List<String>>> resourceClasses) throws ServletException
-	{
+
+	protected Map<Class<?>, ResourceProvider> getResourceProviders(ServletConfig servletConfig, Map<Class<?>, Map<String, List<String>>> resourceClasses) throws ServletException {
 		String scope = servletConfig.getServletContext()
-		                            .getInitParameter("jaxrs.scope");
-		if (scope != null && !"singleton".equals(scope) && !"prototype".equals(scope))
-		{
+				.getInitParameter("jaxrs.scope");
+		if (scope != null && !"singleton".equals(scope) && !"prototype".equals(scope)) {
 			throw new ServletException("Only singleton and prototype scopes are supported");
-		}
-		else
-		{
+		} else {
 			boolean isPrototype = "prototype".equals(scope);
 			Map<Class<?>, ResourceProvider> map = new HashMap();
 
-			for (Map.Entry<Class<?>, Map<String, List<String>>> classMapEntry : resourceClasses.entrySet())
-			{
+			for (Map.Entry<Class<?>, Map<String, List<String>>> classMapEntry : resourceClasses.entrySet()) {
 				Map.Entry<Class<?>, Map<String, List<String>>> entry = classMapEntry;
+                if (bannedResources.contains(entry.getKey().getCanonicalName())) {
+                    continue;
+                }
 				Class<?> c = entry.getKey();
 				map.put(c, isPrototype
-				           ? new PerRequestResourceProvider(c)
-				           : new SingletonResourceProvider(this.createSingletonInstance(c, entry.getValue(), servletConfig), true));
+						? new PerRequestResourceProvider(c)
+						: new SingletonResourceProvider(this.createSingletonInstance(c, entry.getValue(), servletConfig), true));
 			}
 			return map;
 		}
 	}
 
 
-	protected Object createSingletonInstance(Class<?> cls, Map<String, List<String>> props, ServletConfig sc) throws ServletException
-	{
-		try
-		{
+	protected Object createSingletonInstance(Class<?> cls, Map<String, List<String>> props, ServletConfig sc) throws ServletException {
+		try {
 			return GuiceContext.get(cls);
-		}
-		catch (Throwable T)
-		{
+		} catch (Throwable T) {
 			log.log(Level.SEVERE, "Unable to construct instance for cxf", T);
 			Object o = null;
 			try {
 				o = cls.getDeclaredConstructor().newInstance();
 				GuiceContext.inject().injectMembers(o);
 				return o;
-			}catch(Throwable T1)
-			{
-				if(o != null)
-				{
+			} catch (Throwable T1) {
+				if (o != null) {
 					return o;
 				}
-				log.log(Level.FINE,"Unable to create instance for rest :",T1);
+				log.log(Level.FINE, "Unable to create instance for rest :", T1);
 			}
 			return null;
 		}
 	}
 
-	private String getClassNameAndProperties(String cName, Map<String, List<String>> props)
-	{
+	private String getClassNameAndProperties(String cName, Map<String, List<String>> props) {
 		String theName = cName.trim();
 		int ind = theName.indexOf("(");
-		if (ind != -1 && theName.endsWith(")"))
-		{
+		if (ind != -1 && theName.endsWith(")")) {
 			props.putAll(parseMapListSequence(theName.substring(ind + 1, theName.length() - 1)));
 			theName = theName.substring(0, ind)
-			                 .trim();
+					.trim();
 		}
 		return theName;
 	}
 
 	protected Class<?> loadClass(String cName, String classType) throws ServletException {
-		if(cName == null)
+		if (cName == null)
 			return null;
-		return GuiceContext.instance()
-				.getScanResult()
-				.loadClass(cName, true);
-
+		try {
+			Class<?> clazz = GuiceContext.instance()
+					.getScanResult()
+					.loadClass(cName, true);
+			if (clazz == null || Modifier.isAbstract(clazz.getModifiers())) {
+				return null;
+			}
+			return clazz;
+		} catch (Throwable e) {
+			try {
+				Class<?> clazz = Class.forName(cName);
+				if (Modifier.isAbstract(clazz.getModifiers())) {
+					return null;
+				}
+				return clazz;
+			} catch (Throwable e1) {
+				log("Unable to load class - " + cName);
+			}
+		}
+		return null;
 	}
 }
