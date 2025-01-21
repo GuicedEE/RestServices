5473900cf18602031de7b9ac824d25533fd93c75
GedMarc <marc.magon@gmail.com>
2025-01-21 06:26:35 +0200

    Timeout the test calls

--- src/test/java/com/guicedee/guicedservlets/rest/test/RestEasyModuleTest.java
+++ src/test/java/com/guicedee/guicedservlets/rest/test/RestEasyModuleTest.java
@@ -16,32 +16,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RestEasyModuleTest
 {
 
-	@Test
-	void configureServlets() throws Exception
-	{
-		System.out.println("Starting...");
-		IGuiceContext.instance()
-					 .inject();
+    @Test
+    void configureServlets() throws Exception
+    {
+        System.out.println("Starting...");
+        IGuiceContext.instance()
+                .inject();
 
-		//Do stuff
-		HttpClient client = HttpClient.newBuilder()
-									  .connectTimeout(Duration.of(5, ChronoUnit.SECONDS))
-									  .build();
-		HttpResponse response = client.send(HttpRequest.newBuilder()
-													   .GET()
-													   .uri(new URI("http://localhost:8080/rest/hello/world"))
-													   .build(),
-											HttpResponse.BodyHandlers.discarding());
-		assertEquals(200, response.statusCode());
-		response = client.send(HttpRequest.newBuilder()
-										  .GET()
-										  .uri(new URI("http://localhost:8080/rest/hello/helloObject/world"))
-										  .build(),
-							   HttpResponse.BodyHandlers.discarding());
-		assertEquals(200, response.statusCode());
+        //Do stuff
+        HttpClient client = HttpClient.newBuilder()
+                .connectTimeout(Duration.of(5, ChronoUnit.SECONDS))
+                .build();
+        HttpResponse response = client.send(HttpRequest.newBuilder()
+                        .GET()
+                        .uri(new URI("http://localhost:8080/rest/hello/world"))
+                        .timeout(Duration.of(5, ChronoUnit.SECONDS))
+                        .build(),
+                HttpResponse.BodyHandlers.discarding());
+        assertEquals(200, response.statusCode());
+        response = client.send(HttpRequest.newBuilder()
+                        .GET()
+                        .uri(new URI("http://localhost:8080/rest/hello/helloObject/world"))
+                        .timeout(Duration.of(5, ChronoUnit.SECONDS))
+                        .build(),
+                HttpResponse.BodyHandlers.discarding());
+        assertEquals(200, response.statusCode());
 
-		System.out.println("Done");
-		IGuiceContext.get(Vertx.class).close();
-		System.exit(0);
-	}
+        System.out.println("Done");
+        IGuiceContext.get(Vertx.class).close();
+        System.exit(0);
+    }
 }
