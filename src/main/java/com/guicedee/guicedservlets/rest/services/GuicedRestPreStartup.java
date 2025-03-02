ffc6335c06cde89ca9f19add2eb03a2d148aa605
GedMarc <marc.magon@gmail.com>
2025-03-02 21:13:58 +0200

    Group startup items, and use Future's

--- src/main/java/com/guicedee/guicedservlets/rest/services/GuicedRestPreStartup.java
+++ src/main/java/com/guicedee/guicedservlets/rest/services/GuicedRestPreStartup.java
@@ -1,13 +1,20 @@
 package com.guicedee.guicedservlets.rest.services;
 
 import com.guicedee.guicedinjection.interfaces.IGuicePreStartup;
+import com.guicedee.vertx.spi.VertXPreStartup;
 import com.zandero.rest.RestRouter;
+import io.vertx.core.Future;
+
+import java.util.List;
 
 public class GuicedRestPreStartup implements IGuicePreStartup<GuicedRestPreStartup>
 {
     @Override
-    public void onStartup()
+    public List<Future<Boolean>> onStartup()
     {
-        RestRouter.injectWith(new GuiceRestInjectionProvider());
+        return List.of(VertXPreStartup.getVertx().executeBlocking(() -> {
+            RestRouter.injectWith(new GuiceRestInjectionProvider());
+            return true;
+        }));
     }
 }
