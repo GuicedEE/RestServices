3ac27637ede96037f29e83fca6b853728f83786f
GedMarc <ged_marc@hotmail.com>
2026-02-18 00:58:14 +0200

    Vertical's per consumer

--- src/main/java/com/guicedee/guicedservlets/rest/pathing/OperationRegistry.java
+++ src/main/java/com/guicedee/guicedservlets/rest/pathing/OperationRegistry.java
@@ -119,7 +119,7 @@ public class OperationRegistry implements VertxRouterConfigurator<OperationRegis
             // Create route
             router.route(httpMethod, vertxPath).handler(context -> handleRequest(context, resourceInfo, method));
 
-            logger.info("Registered route: " + httpMethod + " " + fullPath);
+            logger.debug("Registered route: " + httpMethod + " " + fullPath);
         } catch (Exception e) {
             logger.error("Error registering resource method: " + method.getName(), e);
         }
